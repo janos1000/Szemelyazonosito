dd3ec7849ea1026e6e705accd2840b7116cdb802
janos1000 <birinyij@kkszki.hu>
2024-10-25 13:01:43 +0200

    Otthon folytattam.

--- Program.cs
+++ Program.cs
@@ -14,6 +14,8 @@ namespace Szemelyazonosito
             var szemszam = Console.ReadLine();
             string szuletes = szemszam.Substring(7, 3);
             int ev = DateTime.Now.Year;
+
+
             if (szemszam[0] % 2 == 0)
             {
                 Console.WriteLine("A személy férfi");
@@ -22,6 +24,7 @@ namespace Szemelyazonosito
 
             Console.WriteLine($"A születési sorszám: {szuletes}");
 
+
             int szulev = 1900 + int.Parse(szemszam.Substring(1, 2));
             if (szemszam[0] == '3' || szemszam[0] == '4')
             {
@@ -29,24 +32,7 @@ namespace Szemelyazonosito
             }
 
             int szulinap = ev - szulev;
-            Console.WriteLine("A {0} szülinapja van.", szulinap);
-
-
-
-            Console.WriteLine("Adjon még egy (10 jegyű) szemszámot: ");
-            var szemszam2 = Console.ReadLine();
-            string szuletes2 = szemszam2.Substring(7, 3);
-            int ev2 = DateTime.Now.Year;
-
-            if szuletes > szuletes2 
-            {
-                Console.WriteLine("Az első ember idősebb");
-            }
-            else if szuletes < szuletes2   
-            {
-                Console.WriteLine();
-            }
-
+            Console.WriteLine("A {0} szülinapját ünnepli.", szulinap);
         }
             static void Main(string[] args)
             {
